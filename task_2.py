7f7b675cd23f68d258449c6affee59bb31aa4fe9
Timur Antonov <antonow.tima@yandex.ru>
2022-09-12 14:10:07 +0300

    agg

--- task_2.py
+++ task_2.py
@@ -1,6 +1,12 @@
 print('world xtefggs')
+<<<<<<< Updated upstream
 def func(a, b):
     return a / b
 print('hi bro')
 print('hah')
 print(222)
+=======
+print('hi bro')
+print('hah')
+print(3 + 10)
+>>>>>>> Stashed changes
