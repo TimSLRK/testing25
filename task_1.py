bdee5ceedf7ec7dc9137a1714cb659676e2e586e
Timur Antonov <antonow.tima@yandex.ru>
2022-09-12 21:12:22 +0300

    added a + b

--- task_1.py
+++ task_1.py
@@ -2,3 +2,4 @@ a = 25
 b = 13
 print(a)
 print(b)
+print(a + b)
--- task_2.py
+++ task_2.py
@@ -1,12 +1,5 @@
 print('world xtefggs')
 print('hi bro')
-<<<<<<< Updated upstream
-print('hah')
-=======
-print('hah')
-print(222)
-print('hi bro')
-print('hah')
-print(3 + 10)
+
 
 
